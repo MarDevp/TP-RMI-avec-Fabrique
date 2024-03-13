fe48624fd93f7e2b9a4e7f19678a2624334859f0
MarDevp <zarroukimariem01@gmail.com>
2024-03-13 17:19:46 +0100

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,4 +1,7 @@
-public class Reverse extends UnicastRemoteObject implements AppInterface {
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+
+public class Reverse extends UnicastRemoteObject implements ReverseInterface {
     public Reverse() throws RemoteException {}
 
     public String reverseString(String input) throws RemoteException {
