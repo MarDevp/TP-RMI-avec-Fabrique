d760006fa284e9a33dad2fdf7a8e21b6ca8d01ba
MarDevp <zarroukimariem01@gmail.com>
2024-03-13 17:12:55 +0100

    Update ReverseInterface.java

--- ReverseInterface.java
+++ ReverseInterface.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ReverseInterface extends Remote {
-    String reverse(String input) throws RemoteException;
+    String reverseString(String input) throws RemoteException;
 }
